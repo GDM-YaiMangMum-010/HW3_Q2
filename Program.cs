eadab140e4545265b492c767b63647dfeeaba3bd
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2022-11-23 23:54:22 +0700

    หั่นผลไม้101

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        int LtoM = 2;
+        int MtoS = 3;
+        bool exit = false;
+        Queue<int> queue = new Queue<int>();
+        while(exit == false)
+        {
+            string friut = Console.ReadLine();
+            switch(friut)
+            {
+                case "L":
+                {
+                    queue.Push(1);
+                }
+                break;
+                case "M":
+                {
+                    queue.Push(2);
+                }
+                break;
+                case "S":
+                {
+                    queue.Push(3);
+                }
+                break;
+                default:
+                {
+                    exit = true;
+                }
+                break;
+            }
+        }
+        while(queue.GetLength()>0)
+        {
+            int ifriut = queue.Pop();
+            Console.Write(ifriut);
+
+            if(ifriut==1)
+            {
+                for(int i = 0;i<LtoM;i++)
+                {
+                    queue.Push(2);
+                }
+            }
+            else if(ifriut==2)
+            {
+                for(int i = 0;i<MtoS;i++)
+                {
+                    queue.Push(3);
+                }
+            }
+        }
+    }
+}
